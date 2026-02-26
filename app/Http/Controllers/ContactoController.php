1581ff34e694b2e4101cb1d4688ca6d7e99a6012
ALGAARLO <algaarlo15@gmail.com>
2026-02-25 20:03:14 -0700

    Crear las rutas para el fomrulario de contacto con controladores:
    -Se crea el contacto.blade.php
    -Se crea el ContactoController con los métodos contacto y recibeFomulario
    -Se agregan las rutas de recibe-formulario y contacto

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactoController extends Controller
+{
+    public function contacto()
+    {
+        return view('contacto');
+    }
+    public function recibeFormulario()
+    {
+         return ('Formulario Recibido');
+    }
+}
